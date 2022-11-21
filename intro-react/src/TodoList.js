052fef731f37812199deceaa30d679c913ffe9d5
Colin <colin25.huart@hotmail.com>
2022-11-21 11:42:02 +0100

    tryImproving

--- intro-react/src/TodoList.js
+++ intro-react/src/TodoList.js
@@ -1,9 +1,26 @@
 import "./TodoList.css";
 import React, { useState } from "react";
+import { useRef } from "react";
 
 function List() {
+  const ref = useRef(null);
+  const handleClick = () => {
+    console.log(ref.current.checked);
+  };
+
+  /* to get all li but error because il.forEach not a functoin????? 
+  
+  import { useRef } from "react";
+let il = document.getElementsByTagName("li");
+function List() {
+  const ref = useRef(null);
+  const handleClick = () => {
+    il.forEach((element) => {
+      console.log(element.current.checked);
+    });
+  };*/
   const initalTodos = [
-    { name: "Be Nice", complete: true },
+    { name: "Be Nice", complete: false },
     { name: "Playing music", complete: false },
   ];
   const [todos, setTodos] = useState(initalTodos);
@@ -12,7 +29,12 @@ function List() {
     <ul className="list">
       {todos.map((todo) => (
         <li key={todo.toString()} className="li">
-          <input type="CheckBox" defaultChecked={todo.complete} />
+          <input
+            ref={ref}
+            type="CheckBox"
+            defaultChecked={todo.complete}
+            onClick={handleClick}
+          />
           {todo.name}
         </li>
       ))}
