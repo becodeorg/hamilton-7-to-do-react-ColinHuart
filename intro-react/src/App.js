da49f1b2406f92ed7084f051a96ddf7be4612dc1
Colin <colin25.huart@hotmail.com>
2022-11-22 15:48:44 +0100

    part4

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -2,10 +2,20 @@ import "./App.css";
 import Title from "./Title";
 import Submit from "./Submit";
 import List from "./TodoList";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+const LSKEY = "MyTodoApp";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem(LSKEY + ".todos");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="background">
